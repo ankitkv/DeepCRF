6a96cb3befb37cce228ef49e0d1f87e790e4096a
Ankit Vani <a@nevitus.org>
2016-03-18 00:11:29 -0400

    Updated dimensions to be more round

--- Configs/example_config.py
+++ Configs/example_config.py
@@ -4,11 +4,13 @@ from os.path import join as pjoin
 # file locations
 git_dir = '/home/jernite/Code/DeepCRF'
 
-data_dir = pjoin(git_dir, 'Data/crf_data_overlaps/')
+data_dir = pjoin(git_dir, 'Data/crf_data/')
+
+log_dir = '/tmp/deepcrf_logs'
 
 train_file = pjoin(data_dir, 'semeval_train/crfpp_text_batch_1.txt')
 dev_file = pjoin(data_dir, 'semeval_dev/crfpp_text_batch_1.txt')
-vecs_file = pjoin(git_dir, 'Data/crf_data_overlaps/semeval_vecs.dat')
+vecs_file = pjoin(git_dir, 'Data/crf_data/semeval_vecs.dat')
 
 vis_file = pjoin(git_dir, 'visual.pickle')
 model_file = pjoin(git_dir, 'recent.model')
@@ -22,9 +24,9 @@ features = ['word', 'lemma', 'pos', 'normal', 'word_length',
 
 tag_list = ['<P>', 'B', 'Bp', 'I', 'Ip', 'In', 'ID', 'O', 'OD']
 
-input_features = collections.OrderedDict({'pos': 7,
-                 'normal': 17, 'prefix': 14, 'suffix': 14,
-                 'all_caps': 1, 'capitalized': 1, 'med_prefix': 13,
+input_features = collections.OrderedDict({'pos': 10,
+                 'normal': 20, 'prefix': 15, 'suffix': 15,
+                 'all_caps': 1, 'capitalized': 1, 'med_prefix': 15,
                  'umls_match_tag_full': 3, 'umls_match_tag_prefix': 3,
                  'umls_match_tag_acro': 3})
 
